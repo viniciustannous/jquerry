8e9dd213b84390b895be294b9a565fb0cad7356e
viniciustannous <160422704+viniciustannous@users.noreply.github.com>
2024-03-11 11:37:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){ 
+    $('form').on('submit', function(e){
+     e.preventDefault(); 
+     const nomeDaTarefa = $('input').val();
+     const novaTarefa = $(`<li>${nomeDaTarefa}</li>`)
+     
+     $(novaTarefa).appendTo('ul')
+     $('input').val('')
+    })
+
+    $('ul').on('click', 'li', function(e){
+        e.preventDefault();
+        $(this).toggleClass('riscarTarefa')
+    })
+})
+
+
